eb493bf317ea533784995a7766ca993b30f70519
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-12-15 20:49:27 +0100

    add eslintcache to ignore

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Form() {
+
+    const Form = () => {
+        return(
+            
+        )
+    }
     return (
         <div>
             <p>Form goes here</p>
