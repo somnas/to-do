d20521dba91dc0fbcfb14948cee4bd211c06a6d0
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-12-15 20:51:37 +0100

    add input form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,19 @@ export default function Form() {
 
     const Form = () => {
         return(
-            
+            <form>
+                <input type="text" class="todo-input" />
+                <button class="todo-button" type="submit">
+                    <i class="fas fa-plus-square"></i>
+                </button>
+                <div class="select">
+                    <select name="todos" class="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                    </select>
+                </div>
+            </form>
         )
     }
     return (
